88dfd1c55b76f8e917b74966f8f6508126b1aa71
mryszawa <makor77@gmail.com>
2021-03-12 18:37:03 +0100

    create userList component

---
+++ src/data/users.js
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Jan Kowalski',
+    attendance: '23%',
+    average: '3.5',
+  },
+  {
+    name: 'Zuzanna Kołek',
+    attendance: '54%',
+    average: '4.6',
+  },
+  {
+    name: 'Bolesław Śmieszek',
+    attendance: '20%',
+    average: '2.9',
+  },
+  {
+    name: 'Zygmunt Waza',
+    attendance: '78%',
+    average: '4.5',
+  },
+  {
+    name: 'Joanna Borek',
+    attendance: '24%',
+    average: '2.1',
+  },
+];
