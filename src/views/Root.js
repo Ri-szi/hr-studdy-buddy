512904d99de82e20c5889a1333f91212296943bd
mryszawa <makor77@gmail.com>
2021-03-17 20:35:04 +0100

    style elements list

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import UsersList from 'components/UsersList/UsersList';
+import UsersList from 'components/organism/UsersList/UsersList';
+import styled, { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'assets/styles/GobalStyle';
+import { theme } from 'assets/styles/theme';
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.lightGrey};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
 
 function Root(props) {
   return (
-    <>
-      <UsersList />
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Wrapper>
+        <UsersList />
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
